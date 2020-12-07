4dcda37c75c21afd68b81bcdf9bacc61484e672f
DFC <69485115+DamieFC@users.noreply.github.com>
2020-12-07 08:31:37 -0800

    Update server_1.rb
    There was no server! 👎

--- server_1.rb
+++ server_1.rb
@@ -1,5 +1,7 @@
 require 'webrick'
 
+server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+
 server.mount_proc '/' do |req, res|
   res.body = 'This is one server powered by Ruby!'
 end
